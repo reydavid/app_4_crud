909e804dfb6892f6e458390ccc9902d3b51752ca
reydavid <5302552+reydavid@users.noreply.github.com>
2021-01-23 23:30:20 -0800

    Table + Read Component

--- client/src/Read.js
+++ client/src/Read.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import {Table} from 'reactstrap';
 
 export class Read extends Component {
     
     render(){
         return (
             <div>
-                <h2>Read</h2>
+                <Table size="sm" variant="dark">
+                    <thead>
+                        <tr>
+hi                            {/* {this.state.name} */}
+                        </tr>    
+                    </thead>
+                </Table>
             </div>
         )
     }
